a4cfd03c5d2c56fc45ee306be9af38f98eda45c0
kgaft <you@example.com>
2024-05-18 23:56:25 +0300

    work on logger

--- src/Logging/Logger.h
+++ src/Logging/Logger.h
@@ -4,11 +4,33 @@
 
 #ifndef LOGGER_H
 #define LOGGER_H
+#include "Threads/Thread.h"
 
+#include <chrono>
 
+enum MessageSeverity {
+    MESSAGE_WARNING,
+    MESSAGE_INFO,
+    MESSAGE_VERBOSE,
+    MESSAGE_ERROR
+};
+
+struct Message {
+    std::string text;
+    std::string source;
+    std::time_t messageTime;
+    MessageSeverity severity;
+    bool processed = false;
+};
 
 class Logger {
+private:
+    static Thread loggerThread;
+    static std::vector<Message*> messages;
+public:
+    static void initializeLogger(const char* logFileName) {
 
+    }
 };
 
 
